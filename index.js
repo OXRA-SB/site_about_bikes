ccb5e0a79925ef17757ea1863ccfa6b6aa1be872
OXRA-SB <vip.ievlev@gmail.com>
2022-08-31 02:00:29 +0400

    site_about_bikes

---
+++ index.js
@@ -0,0 +1,6 @@
+function buttonClick() {
+let a = document.getElementById('inp1')
+let f = document.getElementById('sp1')
+f.innerHTML = a.value
+}
+length
---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.bike__slider, .slayder__items').slick({
+		arrows: false,
+		dots:true,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		
+	})
+});
